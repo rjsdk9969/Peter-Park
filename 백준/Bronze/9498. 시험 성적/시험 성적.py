59ec0f5dfc160fdd9315a5e44e2f9f1938d89372
Kun A Park <113583065+rjsdk9969@users.noreply.github.com>
2022-09-22 00:28:03 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+exam = input()
+exam = int(exam)
+if 100>=int(exam)>=90 :
+    print("A")
+elif 89>=int(exam)>=80 :
+    print("B")
+elif 79>=int(exam)>=70 :
+    print("C")
+elif 69>=int(exam)>=60 :
+    print("D")
+else :
+    print("F")
